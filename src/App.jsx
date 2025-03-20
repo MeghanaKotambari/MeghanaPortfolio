1757a1fa8348d30ce3b7ca4ef99335c648094e6b
test <test@gmail.com>
2025-03-20 16:28:52 +0530

    created navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,25 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import Navbar from './components/Navbar'
+// import background from "./assets/video.mp4"
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+     <div className='w-screen h-screen flex items-center justify-center'>
+      <div className='w-screen h-screen overflow-y-hidden absolute top-0 left-0'>
+      <img src="https://amiralirashidi.github.io/assets/videos/background.png" />
+      {/* <video className='' src={background} autoPlay loop muted></video> */}
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className='h-[700px] w-5/6  bg-gray-900 rounded-3xl bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-40 '>
+
+      <Navbar/>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     </div>
     </>
   )
 }
